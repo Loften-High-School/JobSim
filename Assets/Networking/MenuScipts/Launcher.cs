a39995202583c20a9bb802c116fe85180bfeef18
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2025-10-10 14:42:54 -0400

    10/10

---
+++ Assets/Networking/MenuScipts/Launcher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Launcher : MonoBehaviour
+{
+    public void CreateRoom()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void JoinRoom()
+    {
+        
+        SceneManager.LoadScene(1);
+
+    }
+
+
+}
